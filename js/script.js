3502b48fcd95c23707c9a4ee96309784886d6472
Aryan Jamwal <137585537+jamy677@users.noreply.github.com>
2024-11-12 22:20:02 -0500

    onstructor for the ToyCar object

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function ToyCar(make, model, color, scale, material, features, packaging, price, quantity) {
+    this.make = make;
+    this.model = model;
+    this.color = color;
+    this.scale = scale;
+    this.material = material;
+    this.features = features;
+    this.packaging = packaging;
+    this.price = price;
+    this.quantity = quantity;
+  }
